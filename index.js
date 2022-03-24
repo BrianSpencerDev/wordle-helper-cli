bf299d8fa2788452f1bee6ee019961c26c97a016
Brian Spencer <51007172+BrianSpencerDev@users.noreply.github.com>
2022-03-24 21:03:32 +0000

    Implement crude version of findWordsContaining function

--- index.js
+++ index.js
@@ -2,18 +2,37 @@ const fs = require('fs');
 const readline = require('readline');
 
 const file = 'words.txt';
+let letters = [];
 
 //populate words array 
 const words = fs.readFileSync(file, "utf-8").split("\n");
 
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
 
 //prompt user to enter text
-rl.question(`Enter up to 5 letters`, string => {
-  console.log(string)
-  rl.close()
-})
+// rl.question(`Enter up to 5 letters`, string => {
+//   letters = string.split('');
+//   rl.close()
+// })
+
+
+console.log(findWordsContaining(['h', 'o'], words));
+
+function findWordsContaining(arr1, arr2){
+  const wordsWith = [];
+
+  for (const letter of arr1){
+    for(const word of arr2){
+      if (word.includes(letter) && !wordsWith.includes(word)){
+        wordsWith.push(word);
+        console.log('hi')
+      }
+    }
+  }
+  
+  return wordsWith;
+}
