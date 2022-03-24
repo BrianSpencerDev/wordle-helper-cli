1e321621c51515efa442d7ee697b284668f0284c
Brian Spencer <51007172+BrianSpencerDev@users.noreply.github.com>
2022-03-24 20:29:09 +0000

    Letter input

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
+const readline = require('readline');
 
 const file = 'words.txt';
 
 //populate words array 
 const words = fs.readFileSync(file, "utf-8").split("\n");
 
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+//prompt user to enter text
+rl.question(`Enter up to 5 letters`, string => {
+  console.log(string)
+  rl.close()
+})
