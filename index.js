05e5ca111e90e329ec358256e0de207064017bb6
Brian Spencer <51007172+BrianSpencerDev@users.noreply.github.com>
2022-03-25 18:56:48 +0000

    Improve findWordsContaining function

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const fs = require('fs');
-const readline = require('readline');
+const prompt = require('prompt-sync')({sigint: true});
 
 const file = 'words.txt';
-let letters = [];
 
 //populate words array 
 const words = fs.readFileSync(file, "utf-8").split("\n");
 
+//get user input
+const input = prompt('Enter up to 5 letters:  ');
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// })
+//convert input string to array of letters
+const letters = input.split('');
 
-//prompt user to enter text
-// rl.question(`Enter up to 5 letters`, string => {
-//   letters = string.split('');
-//   rl.close()
-// })
+console.log(findWordsContaining(letters, words));
 
 
-console.log(findWordsContaining(['h', 'o'], words));
 
 function findWordsContaining(arr1, arr2){
+  let pattern = '';
   const wordsWith = [];
 
+  //create regex
   for (const letter of arr1){
-    for(const word of arr2){
-      if (word.includes(letter) && !wordsWith.includes(word)){
-        wordsWith.push(word);
-        console.log('hi')
-      }
-    }
+    pattern += '(?=\\w*' + letter + ')';
   }
+  pattern += '\\w+';
+  const regex = new RegExp(pattern);
+
+  //iterate through words to see if letters are in a word
+  for (const word of words){
+    //if letters are in word add word to wordsWith arr
+    if(regex.test(word)){
+      wordsWith.push(word)
+    }
+  };
   
   return wordsWith;
 }
